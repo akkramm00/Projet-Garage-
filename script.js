39c5e7a2a241e092b7d642a2658f777154fdc033
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-06 10:31:33 +0200

    add all modif

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+// Fonction pour afficher la modal
+function ouvrirModal() {
+  var modal = document.getElementById('myModal');
+  modal.style.display = 'block';
+}
+
+// Fonction pour fermer la modal
+function fermerModal() {
+  var modal = document.getElementById('myModal');
+  modal.style.display = 'none';
+}
