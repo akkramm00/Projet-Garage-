3513dac0c62a44010139f36d86acf3f12b9386d6
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-11 01:23:13 +0200

    commit all.

--- script.js
+++ script.js
@@ -11,3 +11,37 @@ function fermerModal() {
   var modal = document.getElementById('myModal');
   modal.style.display = 'none';
 }
+
+// validation formulaire
+
+function validerFormulaire() {
+  var nom = document.getElementById('nom').value.trim();
+  var email = document.getElementById('email').value.trim();
+  var motDePasse = document.getElementById('motDePasse').value.trim();
+  var genre = document.querySelector('input[name="genre"]:checked');
+  var adresse = document.getElementById('adresse').value.trim();
+  var commentaire = document.getElementById('commentaire').value.trim();
+
+  // Vérifier si les champs sont vides
+  if (nom === '' || email === '' || motDePasse === '' || !genre || adresse === '' || commentaire === '') {
+    alert("Veuillez remplir tous les champs !");
+    return false;
+  }
+
+  // Vérifier si l'adresse email est valide
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    alert("Veuillez entrer une adresse email valide !");
+    return false;
+  }
+
+  // Vérifier si le mot de passe a au moins 6 caractères
+  if (motDePasse.length < 6) {
+    alert("Le mot de passe doit contenir au moins 6 caractères !");
+    return false;
+  }
+
+  // Ajoutez vos propres conditions de validation supplémentaires ici
+
+  return true;
+}
